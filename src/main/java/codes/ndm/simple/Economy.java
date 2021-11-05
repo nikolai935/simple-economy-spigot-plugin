7eab8f174dff784d396e2a658c307a939c7170d1
nikolai935 <nikolai@milde.nu>
2021-11-05 13:08:20 +0100

    Made the connection to the database

--- src/main/java/codes/ndm/simple/Economy.java
+++ src/main/java/codes/ndm/simple/Economy.java
@@ -1,17 +1,44 @@
 package codes.ndm.simple;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.UuidRepresentation;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Economy extends JavaPlugin {
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> userCollection;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        connect();
 
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Plugin Enabled");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        mongoClient.close();
+        Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Plugin Disabled!");
+    }
+
+    public void connect() {
+        ConnectionString connectionString = new ConnectionString(getConfig().getString("conn"));
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString).uuidRepresentation(UuidRepresentation.STANDARD)
+                .build();
+        mongoClient = MongoClients.create(settings);
+        database = mongoClient.getDatabase(getConfig().getString("database"));
+        userCollection = database.getCollection("economy");
     }
 }
