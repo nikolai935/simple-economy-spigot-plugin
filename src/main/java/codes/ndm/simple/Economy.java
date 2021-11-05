e17a162c21450f5086b3b97cc7fa951b5112af4d
nikolai935 <nikolai@milde.nu>
2021-11-05 13:04:42 +0100

    Initial commit

---
+++ src/main/java/codes/ndm/simple/Economy.java
@@ -0,0 +1,17 @@
+package codes.ndm.simple;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Economy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
